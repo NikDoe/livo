dfd4db09acbdc2a1dedd8717a7c446ef7562b877
Nikita <80026327+NikDoe@users.noreply.github.com>
2024-09-12 19:14:58 +0300

    Update page.tsx (#74)
    * Update page.tsx
    
    * Update page.tsx
    
    ---------
    
    Co-authored-by: NikDoeSecondAccount <ValyaKuzmenko63@yandex.ru>

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -5,6 +5,8 @@ type UserProfileProps = {
 }
 
 function UserProfile({ params }: UserProfileProps) {
+	console.log('test github achivments👽');
+	
 	return (
 		<h1>
 			Пользователь - {params.userId}
