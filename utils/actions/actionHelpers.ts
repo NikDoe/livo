34ba51774fadc1f45310760756a8fbe37afab754
Nikita <80026327+NikDoe@users.noreply.github.com>
2024-09-15 11:36:36 +0300

    feat: add auth helper for route protection and profile redirection (#91)
    - Added a helper to check if a user is authorized.
    - Blocks access to certain routes if the user is not authorized.
    - Redirects users to the profile creation page if they do not have a profile in the database.

--- utils/actions/actionHelpers.ts
+++ utils/actions/actionHelpers.ts
@@ -1,3 +1,6 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
 type ErrorResponse = {
 	message: string;
 }
@@ -6,4 +9,16 @@ export const renderError = (error: unknown): ErrorResponse => {
 	return {
 		message: error instanceof Error ? error.message : 'Произошла ошибка, попробуйте снова',
 	};
+};
+
+export const getAuthUser = async () => {
+	const user = await currentUser();
+
+	if (!user) {
+		throw new Error('Для доступа к этой странице необходимо войти в аккаунт');
+	}
+
+	if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+
+	return user;
 };
