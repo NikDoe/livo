1f8238204db6a8dffc39f1c7193323726a7546d8
NikDoe <nikd0e@yandex.ru>
2024-10-09 11:21:11 +0300

    feat: add metadata to single stay page

--- app/stays/[stayId]/page.tsx
+++ app/stays/[stayId]/page.tsx
@@ -1,12 +1,27 @@
 import { PageContainer, ShareButton } from '@/components/common';
 import { FavoriteToggleButton } from '@/components/mainPages';
 import { fetchStayDetails } from '@/utils/actions';
+import { type Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 type SingleStayPageProps = {
 	params: {
 		stayId: string
 	}
+}
+
+export async function generateMetadata({ params }: SingleStayPageProps): Promise<Metadata> {
+	const stay = await fetchStayDetails(params.stayId);
+
+	const stayTitle = stay?.stayTitle;
+	const stayDescription = 'Узнайте больше о данном жилье, включая описание, стоимость аренды, удобства и местоположение.';
+	const stayKeywords = ['жилье', 'аренда', 'жилье', 'недвижимость', 'квартира'];
+
+	return {
+		title: stayTitle,
+		description: stayDescription,
+		keywords: stayKeywords,
+	};
 }
 
 async function SingleStayPage({ params }: SingleStayPageProps) {
